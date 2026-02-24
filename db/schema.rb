9693fc847c0354ee75e1e93d4fb9c6703402512f
ScribeK2 <kkenney.official@gmail.com>
2026-02-24 08:34:35 -0500

    fix: commit schema.rb after rich text migration
    The migration added in 8d71b5f was committed without the
    corresponding schema.rb update, causing CI to fail with
    "Migrations are pending."

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_02_22_112120) do
+ActiveRecord::Schema[8.0].define(version: 2026_02_24_131447) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -169,6 +169,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_02_22_112120) do
     t.index ["is_public"], name: "index_workflows_on_is_public"
     t.index ["status", "user_id"], name: "index_workflows_on_status_and_user_id"
     t.index ["status"], name: "index_workflows_on_status"
+    t.index ["steps"], name: "index_workflows_on_steps"
     t.index ["user_id"], name: "index_workflows_on_user_id"
   end
 
