c1478965fc567f802cb535b597b31777b9374eb8
ScribeK2 <kkenney.official@gmail.com>
2026-03-23 09:37:14 -0400

    feat: add template picker popover to builder toolbar

---
+++ app/javascript/controllers/template_picker_controller.js
@@ -0,0 +1,55 @@
+// app/javascript/controllers/template_picker_controller.js
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["popover"]
+  static values = { workflowId: Number }
+
+  toggle() {
+    this.popoverTarget.hidden = !this.popoverTarget.hidden
+  }
+
+  close() {
+    this.popoverTarget.hidden = true
+  }
+
+  apply(event) {
+    const key = event.currentTarget.dataset.templateKey
+    const name = event.currentTarget.dataset.templateName
+
+    if (!confirm(`This will replace your current steps with the "${name}" template. Continue?`)) {
+      return
+    }
+
+    this.close()
+
+    const url = `/workflows/${this.workflowIdValue}/steps/apply_template`
+    const token = document.querySelector('meta[name="csrf-token"]')?.content
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "X-CSRF-Token": token,
+        "Accept": "text/vnd.turbo-stream.html"
+      },
+      body: new URLSearchParams({ template_key: key })
+    })
+    .then(response => response.text())
+    .then(html => Turbo.renderStreamMessage(html))
+  }
+
+  closeOnOutsideClick = (event) => {
+    if (!this.element.contains(event.target)) {
+      this.close()
+    }
+  }
+
+  connect() {
+    document.addEventListener("click", this.closeOnOutsideClick)
+  }
+
+  disconnect() {
+    document.removeEventListener("click", this.closeOnOutsideClick)
+  }
+}
